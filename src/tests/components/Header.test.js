e7228b656b4bb6a78637bd652e2fe57a50cdcd15
Can Yener <can.yener@hotmail.com.tr>
2020-05-18 14:16:11 +0300

    Revert "Implement ci"

--- src/tests/components/Header.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Header from '../../components/Header'
-
-test('Should render Header correctly', () => {
-  const wrapper = shallow(<Header />)
-  expect(wrapper).toMatchSnapshot()
-})
