be4f9e7b82058c5a7f016d83c893063847ea5fd8
canyener <canyener1337@gmail.com>
2020-05-18 12:23:32 +0300

    Adds snapshot test for Header component

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Header from '../../components/Header'
+
+test('Should render Header correctly', () => {
+  const wrapper = shallow(<Header />)
+  expect(wrapper).toMatchSnapshot()
+})
